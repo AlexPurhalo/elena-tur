7a8c784dc82f23b8b3ed255e0f68e9115ac0e671
alexpurhalo <alexpurhalo@gmail.com>
2016-06-08 12:41:42 +0300

    truncate-&-simple_format-main-page

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,5 +1,5 @@
 class Tour < ActiveRecord::Base
-  has_attached_file :cover, :styles => { :medium => "550x271#", :thumb => "100x100>", large: "500x500>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :cover, :styles => { :medium => "548x300#", :thumb => "100x100>", large: "500x500>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
   validates :cover, :title, :description, presence: true
 end
