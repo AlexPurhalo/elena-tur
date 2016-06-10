2a2a9a33bd47e8762304adc1befc1ab0be4b7c0a
alexpurhalo <alexpurhalo@gmail.com>
2016-06-10 18:56:48 +0300

    admin-user-permissions

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,5 +1,7 @@
 class ToursController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :create_permission, only: [:new, :create]
+  before_action :edit_permission, only: [:edit, :update, :destroy]
 
   def index
     @tours = Tour.order("created_at DESC")
@@ -38,9 +40,35 @@ class ToursController < ApplicationController
     redirect_to root_path
   end
 
+  def create_permission
+    @tour = current_user.tours.build
+
+    unless @tour.user.admin?
+      flash[:notice] = 'Only administration can add posts'
+      redirect_to root_path
+    end
+  end
+
+  def edit_permission
+    @tour = Tour.find(params[:id])
+
+    unless @tour.user == current_user
+      flash[:notice] = 'Only administration can rule by posts'
+      redirect_to @tour
+    end
+  end
+
   private
   def tour_params
     params.require(:tour).permit(:cover, :title, :description, :date, :price, :money, :place)
   end
 end
+
+
+
+
+
+
+
+
 
