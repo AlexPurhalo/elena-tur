1690fb4b4dd73c9fec3c564fe485b06120df4869
alexpurhalo <alexpurhalo@gmail.com>
2016-07-30 01:10:52 +0300

    vk-auth-bug-fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.name = auth.info.name
       user.image = auth.info.image
+      user.email = auth.uid
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
       user.password = Devise.friendly_token[0,20]
